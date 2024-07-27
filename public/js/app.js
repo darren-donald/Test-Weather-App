abf8eeb6b267d537c2f20effac34d2d25287dc7a
Darren Donald <darrend@sshw.com.au>
2024-07-27 20:52:31 +1000

    Testing the commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,8 @@
 
-console.log('Yep works')
+// console.log('Yep works')
 
     
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-                
-//         }
-//     })
-// })
+
 
 const weatherForm = document.querySelector('form')
 const searchString = document.querySelector('input')
